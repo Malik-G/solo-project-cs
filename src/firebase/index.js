220f618b9e3573b75a9600540882a80c825d86df
Malik-G <Malik-G@users.noreply.github.com>
2018-11-20 05:31:27 -0600

    reconfigured UserPage to be a class component vs a function component

---
+++ src/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/storage'
+
+// Initialize Firebase
+var config = {
+   apiKey: "AIzaSyDdi3EMOD03Z9PTBn_HBtjikc42aMvXtMM",
+   authDomain: "card-swap-72684.firebaseapp.com",
+   databaseURL: "https://card-swap-72684.firebaseio.com",
+   projectId: "card-swap-72684",
+   storageBucket: "card-swap-72684.appspot.com",
+   messagingSenderId: "30007662181"
+ };
+ 
+ firebase.initializeApp(config);
+ 
+ const storage = firebase.storage();
+
+ export {
+    storage, firbase as default
+ }
